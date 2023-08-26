9e6e891131a475fb3476413deb8471a66ba0987a
smaacstem <140139296+smaacstem@users.noreply.github.com>
2023-08-27 02:02:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express=require("express");
-var bodyParser=require("body-parser");
+var bodyParser=require("body-parser");//handle the request given in req.body
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://sandhiya:ltjLc8vIK4rMo92H@cluster0.c4a7zni.mongodb.net/stem',{useNewUrlParser: true});
+const mongoose = require('mongoose'); //initiating mongoose
+mongoose.connect('mongodb+srv://sandhiya:ltjLc8vIK4rMo92H@cluster0.c4a7zni.mongodb.net/stem',{useNewUrlParser: true}); //get connection link from mongodb
 var db=mongoose.connection;
 db.on('error', console.log.bind(console, "connection error"));
 db.once('open', function(callback){
@@ -19,11 +19,11 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.post('/log_in', function(req,res){
-	var junioruserName = req.body.junioruserName;
+	var junioruserName = req.body.junioruserName; // get the name of the user input field in HTML
 	var data = {
-		"junioruserName": junioruserName
+		"junioruserName": junioruserName //show user iput vaue as (Eg:- juniorusername: Lisa) in Mongodb
 	}
-db.collection('login').insertOne(data,function(err, collection){
+db.collection('login').insertOne(data,function(err, collection){ //Insert user input value into mongodb collection.
 		if (err) throw err;
 		console.log("Record inserted Successfully");
 			
@@ -35,7 +35,7 @@ db.collection('login').insertOne(data,function(err, collection){
 
 app.get('/',function(req,res){
 res.set({
-	'Access-control-Allow-Origin': '*'
+	'Access-control-Allow-Origin': '*'  //To share the requested details.(please google)
 	});
 // return res.redirect('https://smaacstem.github.io/');
 }).listen(3000)
